412c985fb0d11d2dc4ffd3d680a21b163fe85cfc
agsawyer <alyssa@vbm.com>
2023-06-12 15:25:26 -0700

    renaming ch5 results (0shot)

--- eval-model-pt3/rename.sh
+++ eval-model-pt3/rename.sh
@@ -1,4 +1,7 @@
-for file in "results-capV"/*; do
-    # Rename the file
-    mv "$filename" "${filename%.json}.0shot.json"
+
+folder_path="results-capV"
+
+for file in "$folder_path"/*.json; do
+    new_filename=$(echo "$file" | sed 's/\.json$/.0shot.json/')
+    mv "$file" "$new_filename"
 done

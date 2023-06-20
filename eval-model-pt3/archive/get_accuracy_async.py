dfb72b0f4ad3cecf035df3d190be158fef42e6e6
agsawyer <alyssa@vbm.com>
2023-06-20 16:57:52 -0700

    trying to make get accuracy sync

--- eval-model-pt3/archive/get_accuracy_async.py
+++ eval-model-pt3/archive/get_accuracy_async.py
@@ -7,18 +7,16 @@ import argparse
 import aiometer
 from functools import partial
 
-def query(c):
+async def query(c):
     '''takes in a dict that includes lmql prompt and the true answer,
     returns 1 if the model is correctand 0 if wrong'''
     print("sending output")
-    output = lmql.run_sync(c["code"], output_writer=lmql.stream("RESPONSE"))
+    output = (await lmql.run(c["code"], output_writer=lmql.stream("RESPONSE")))
     print(output)
     return {"code":c["code"]}, {"answer":c["answer"]}, {"model_output":output[0].variables['ANSWER'].strip()}
 
-def run(codes):
-    results = []
-    for c in codes:
-        results.append(query(c))
+async def run(codes):
+    results = await aiometer.run_all([partial(query,c) for c in codes], max_per_second=.2, max_at_once=5)
     return results
 
 def get_outputs(codes):
@@ -81,9 +79,9 @@ def main():
     output_response_file = "x/" +  json_name + ".raw.json"
 
     #if not os.path.exists(output_accuracy_file) or os.path.getsize(file_path) == 0:
-    model_output = get_outputs(data["codes"]) 
-    print(model_output)
-   # model_output = [({'code': 'argmax \'ANSWER KEY:[āte, am ant ī ōs at īte, it am iunt, iunt ō īs īs, ē ās ēte, īs, ā ā it, ent, ēte ite ās ite, ās unt unt ō am unt ō ās ā unt] Q: lius et Aemilia in vīll~ habit~ cum liber~ et serv~. A: ā ant īs īs Q: minus mult~ serv~ et mult~ ancill~ habet. A: ōs ōs ās ās Q: milia in peristȳl~ est cum Mārc~ et Quīnt~ et Iūli~. A: ō ō ō ā Q: Aemilia: "Mārce et Quīnte! Voc~ Iūliam!" A: [ANSWER]\' from \'openai/davinci\' where ANSWER in [\'ā\', \'ōs\', \'ō\', \'ās\', \'am\', \'āte\', \'am\', \'it\', \'iunt\', \'ē\', \'īs\', \'ā\', \'ent\', \'ēte\', \'ās\']'}, {'answer': 'āte'}, {'model_output': 'īs'}), ({'code': 'argmax \'ANSWER KEY:[āte, am ant ī ōs at īte, it am iunt, iunt ō īs īs, ē ās ēte, īs, ā ā it, ent, ēte ite ās ite, ās unt unt ō am unt ō ās ā unt] Q: lius et Aemilia in vīll~ habit~ cum liber~ et serv~. A: ā ant īs īs Q: minus mult~ serv~ et mult~ ancill~ habet. A: ōs ōs ās ās Q: milia in peristȳl~ est cum Mārc~ et Quīnt~ et Iūli~. A: ō ō ō ā Q: Puerī Iūli~ voc~ : "Iūlia! Ven~!" et Iūlia puer~ voc~ : "Mārce et Quīnte! Ven~ " A: [ANSWER]\' from \'openai/davinci\' where ANSWER in [\'ā\', \'ōs\', \'ō\', \'ās\', \'am\', \'āte\', \'am\', \'it\', \'iunt\', \'ē\', \'īs\', \'ā\', \'ent\', \'ēte\', \'ās\']'}, {'answer': 'am ant ī ōs at īte'}, {'model_output': 'īs'})]
+    #model_output = get_outputs(data["codes"]) TODO: FIX
+    #print(model_output)
+    model_output = [({'code': 'argmax \'ANSWER KEY:[āte, am ant ī ōs at īte, it am iunt, iunt ō īs īs, ē ās ēte, īs, ā ā it, ent, ēte ite ās ite, ās unt unt ō am unt ō ās ā unt] Q: lius et Aemilia in vīll~ habit~ cum liber~ et serv~. A: ā ant īs īs Q: minus mult~ serv~ et mult~ ancill~ habet. A: ōs ōs ās ās Q: milia in peristȳl~ est cum Mārc~ et Quīnt~ et Iūli~. A: ō ō ō ā Q: Aemilia: "Mārce et Quīnte! Voc~ Iūliam!" A: [ANSWER]\' from \'openai/davinci\' where ANSWER in [\'ā\', \'ōs\', \'ō\', \'ās\', \'am\', \'āte\', \'am\', \'it\', \'iunt\', \'ē\', \'īs\', \'ā\', \'ent\', \'ēte\', \'ās\']'}, {'answer': 'āte'}, {'model_output': 'īs'}), ({'code': 'argmax \'ANSWER KEY:[āte, am ant ī ōs at īte, it am iunt, iunt ō īs īs, ē ās ēte, īs, ā ā it, ent, ēte ite ās ite, ās unt unt ō am unt ō ās ā unt] Q: lius et Aemilia in vīll~ habit~ cum liber~ et serv~. A: ā ant īs īs Q: minus mult~ serv~ et mult~ ancill~ habet. A: ōs ōs ās ās Q: milia in peristȳl~ est cum Mārc~ et Quīnt~ et Iūli~. A: ō ō ō ā Q: Puerī Iūli~ voc~ : "Iūlia! Ven~!" et Iūlia puer~ voc~ : "Mārce et Quīnte! Ven~ " A: [ANSWER]\' from \'openai/davinci\' where ANSWER in [\'ā\', \'ōs\', \'ō\', \'ās\', \'am\', \'āte\', \'am\', \'it\', \'iunt\', \'ē\', \'īs\', \'ā\', \'ent\', \'ēte\', \'ās\']'}, {'answer': 'am ant ī ōs at īte'}, {'model_output': 'īs'})]
     with open(output_accuracy_file, "w") as outfile:
        outfile.write(str(calculate_accuracy(model_output)))
     with open(output_response_file, "w") as outfile:

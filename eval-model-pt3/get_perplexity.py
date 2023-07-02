80c5add45702e713735b79578ff6d0a6dfd462b3
agsawyer <alyssa@vbm.com>
2023-07-01 20:50:44 -0700

    perplexity code finished

--- eval-model-pt3/get_perplexity.py
+++ eval-model-pt3/get_perplexity.py
@@ -7,6 +7,7 @@ import os
 import openai
 
 def get_perplexity(model, sentence):
+    '''queries openai api to get logprobs of the prompt sentence'''
 
     # define the completion parameters
     completion_parameters = {
@@ -19,120 +20,98 @@ def get_perplexity(model, sentence):
 
     # calling openai api to get completion response
     response = openai.Completion.create(**completion_parameters)
-
     # extracing the log probabilities 
     choices = response['choices'][0]
     token_logprobs = choices['logprobs']['token_logprobs']
 
-    # converting into perplexity
-    l = sum(token_logprobs[1:]) / len(token_logprobs[1:])
-    perplexity = 2 ** (-l)
+    return token_logprobs
 
-    return perplexity
-
-def get_outputs(codes):
-    '''passes lmql code to query, returns a list of dictionaries of all of the model's results'''
+def evaluate_questions(questions):
+    '''takes in model output json data, returns a list of 1s and 0s, where 1s represent correct answers'''
     results = []
 
-    # for one set of questions in the file
-    for question in codes:
-        results.append(score_perplexity(question))
-    
-    print(results)
+    # iterate thru questions
+    for question in questions:
+        lowest_perplexity = float('inf')
+        lowest_perplexity_index = -1
+        for i, evalsentence in enumerate(question['evalsentences']):
+            perplexity = evalsentence['perplexity']  # Extract perplexity from the evaluated sentence
+
+            # flagging if lowest perplexity
+            if perplexity < lowest_perplexity:
+                lowest_perplexity = perplexity
+                lowest_perplexity_index = i
+
+        # seeing if the answer is correct, if lowest perplexity sentence is the same as the answer
+        is_correct = 1 if question['evalsentences'][lowest_perplexity_index]['evalsentence'] == question['answer'] else 0
+        results.append(is_correct)
+
     return results
-
-
-
-def score_perplexity(model_output):
-    '''takes in list that contains the model output, questions, and answers, returns accuracy score'''
-
-    # takes in question and possible questions
-
-    #  check whcih one is max
-
-    # if that is correct, score 1 for the question 
-
-    # keeps the perplexity of the correct answer
-
-
-    return sum(scores) / len(scores)
-
-    scores = []
-    for question in model_output:
-        if question[1]["answer"] == question[2]["model_output"]:
-            scores.append(1)
-        else:
-            scores.append(0)
-    
-    return sum(scores) / len(scores)
 
 
 # getting json name
 parser = argparse.ArgumentParser()
 parser.add_argument('second_argument')
 # opening json file
-file_path = Path.cwd()/parser.parse_args().second_argument
-with file_path.open(mode='r',encoding="utf-8") as f:
-        data = json.load(f)
+file_path = Path.cwd() / parser.parse_args().second_argument
+with file_path.open(mode='r', encoding="utf-8") as f:
+    data = json.load(f)
 
 
-model = 'davinci'
+# creating output base filename
+info_list = parser.parse_args().second_argument.split(".")
+model = info_list[4]
+
+json_name = ".".join([info_list[0].split("/")[2], info_list[5], info_list[1], model, info_list[0].split("/")[1], info_list[2], info_list[3]])
+
+# creating output filepaths
+output_perplexity_file = "results/results-cap1to5/" + json_name + ".json"
+output_response_file = "results/results-cap1to5-raw/" +  json_name + ".raw.json"
 
 
-# Iterate through questions
-for item in data:
-    question = item["question"]
-    evalsentences = item["evalsentences"]
-    answer = item["answer"]
+# only running new code 
+if not os.path.exists(output_response_file) or os.path.getsize(output_response_file) == 0: 
     
-    print("Question:", question)
-    
-    # Iterate through evalsentences
-    result_list = []
-    for sentence in evalsentences:
-        # Evaluate each sentence and calculate perplexity
-        perplexity = get_perplexity(model, sentence)
-        
-        # Create a dictionary with evaluated sentence and perplexity
-        result = {
-            "evalsentence": sentence,
-            "perplexity": perplexity
-        }
-        
-        # Append the result to the list
-        result_list.append(result)
-    
-    # Update the evalsentences key with the result list
-    item["evalsentences"] = result_list
-    
-    print("Result:", json.dumps(item, ensure_ascii=False, indent=4))
-    
-# Save the updated data to a JSON file
-with open("output.json", "w") as outfile:
-    json.dump(data, outfile, ensure_ascii=False, indent=4)
+    # Iterate through questions
+    for item in data:
+        question = item["question"]
+        evalsentences = item["evalsentences"]
+        answer = item["answer"]
+
+        print("Question:", question)
+
+        # Iterate through evalsentences
+        result_list = []
+        for sentence in evalsentences:
+            # Evaluate each sentence and calculate perplexity
+            log_probs = get_perplexity(model, sentence)
+
+            # Calculate perplexity from log_probs
+            l = sum(log_probs[1:]) / len(log_probs[1:])
+            perplexity = 2 ** (-l)
+
+            # Create a dictionary with evaluated sentence, perplexity, and log_probs
+            result = {
+                "evalsentence": sentence,
+                "perplexity": perplexity,
+                "log_probs": log_probs
+            }
+
+            # Append the result to the list
+            result_list.append(result)
+
+        # Update the evalsentences key with the result list
+        item["evalsentences"] = result_list
+
+        print("Result:", json.dumps(item, ensure_ascii=False, indent=4))
+
+    with open(output_response_file, "w") as outfile:
+        outfile.write(json.dumps(data))
+
+    # Evaluate the questions based on perplexity and print the results
+    results = evaluate_questions(data)
+    accuracy = sum(results) / len(results)
+    with open(output_perplexity_file, "w") as outfile:
+        outfile.write(str(accuracy))
 
 
-# # creating output base filename
-# info_list = parser.parse_args().second_argument.split(".")
-# model = info_list[4]
-
-# json_name = ".".join([info_list[0].split("/")[2], info_list[5], info_list[1], model, info_list[0].split("/")[1], info_list[2], info_list[3]])
-
-# # creating output filepaths
-# output_accuracy_file = "results/results-cap1to3/" + json_name + ".json"
-# output_response_file = "results/results-cap1to3-raw/" +  json_name + ".raw.json"
-
-# # only running new code 
-# if not os.path.exists(output_accuracy_file) or os.path.getsize(file_path) == 0: 
-#     # # getting model output
-#     # model_output = get_outputs(data["codes"]) 
-
-#     # # to NOT re query -- replace above line with: 
-#     # # with open(output_response_file, 'r') as file:
-#     # #     model_output =  json.load(file)
-
-#     # # exporting model output
-#     # with open(output_accuracy_file, "w") as outfile:
-#     #     outfile.write(str(calculate_accuracy(model_output)))
-#     # with open(output_response_file, "w") as outfile:
-#     #     outfile.write(json.dumps(model_output))
